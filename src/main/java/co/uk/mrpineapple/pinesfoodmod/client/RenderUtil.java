615b2ec12edfa00d7e047d336108f3ab79c6e316
Mr-Pineapple <zackbird1@outlook.com>
2021-08-05 15:20:22 +0100

    :recycle: Refactored render method names

--- src/main/java/co/uk/mrpineapple/pinesfoodmod/client/RenderUtil.java
+++ src/main/java/co/uk/mrpineapple/pinesfoodmod/client/RenderUtil.java
@@ -6,8 +6,12 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
 public class RenderUtil {
-    public static IBakedModel getModel(Item item) {
+    public static IBakedModel getModelFromItem(Item item) {
         return Minecraft.getInstance().getItemRenderer().getItemModelShaper().getItemModel(new ItemStack(item));
+    }
+
+    public static IBakedModel getModelFromItemStack(ItemStack itemstack) {
+        return Minecraft.getInstance().getItemRenderer().getItemModelShaper().getItemModel(itemstack);
     }
 
     public static boolean isMouseWithin(int mouseX, int mouseY, int x, int y, int width, int height) {
