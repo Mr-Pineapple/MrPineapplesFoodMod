a66891e74fffdaef9e29f69d4c47663035e7db4d
Mr-Pineapple <zackbird1@outlook.com>
2021-09-01 22:06:53 +0100

    :link: Linked block to tile entity

--- src/main/java/co/uk/mrpineapple/pinesfoodmod/core/registry/TileEntityRegistry.java
+++ src/main/java/co/uk/mrpineapple/pinesfoodmod/core/registry/TileEntityRegistry.java
@@ -18,7 +18,7 @@ public class TileEntityRegistry {
 
     public static final RegistryObject<TileEntityType<PizzaBoardTileEntity>> PIZZA_BOARD = register("pizza_board", PizzaBoardTileEntity::new, () -> new Block[]{BlockRegistry.PIZZA_BOARD_ACACIA.get(), BlockRegistry.PIZZA_BOARD_BIRCH.get(), BlockRegistry.PIZZA_BOARD_DARK_OAK.get(), BlockRegistry.PIZZA_BOARD_JUNGLE.get(), BlockRegistry.PIZZA_BOARD_OAK.get(), BlockRegistry.PIZZA_BOARD_SPRUCE.get()});
     public static final RegistryObject<TileEntityType<PizzaOvenTileEntity>> PIZZA_OVEN = register("pizza_oven", PizzaOvenTileEntity::new, () -> new Block[]{BlockRegistry.PIZZA_OVEN.get()});
-    public static final RegistryObject<TileEntityType<PizzaBoxTileEntity>> PIZZA_BOX = register("pizza_box", PizzaBoxTileEntity::new, () -> new Block[]{});
+    public static final RegistryObject<TileEntityType<PizzaBoxTileEntity>> PIZZA_BOX = register("pizza_box", PizzaBoxTileEntity::new, () -> new Block[]{BlockRegistry.PIZZA_BOX.get()});
 
     private static <T extends TileEntity>RegistryObject<TileEntityType<T>> register(String id, Supplier<T> factory, Supplier<Block[]> validBlocks) {
         return TILE_ENTITY.register(id, () -> TileEntityType.Builder.of(factory, validBlocks.get()).build(null));
